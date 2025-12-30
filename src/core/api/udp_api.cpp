8d4b7ec1bd332b4ea348a8d89bd4d5ec061edae1
Marshall Clyburn <mardev2013@gmail.com>
2025-12-30 12:06:33 -0500

    Construct plaintext AAD
    Adds buildout of the AAD for the OSCORE message. Also adds the loop
    to filter through CoAP options.

--- src/core/api/udp_api.cpp
+++ src/core/api/udp_api.cpp
@@ -33,6 +33,7 @@
 
 #include "openthread-core-config.h"
 
+#include <openthread/coap.h>
 #include <openthread/udp.h>
 
 #include "common/as_core_type.hpp"
@@ -70,19 +71,95 @@ otError otUdpConnect(otInstance *aInstance, otUdpSocket *aSocket, const otSockAd
     return AsCoreType(aInstance).Get<Ip6::Udp>().Connect(AsCoreType(aSocket), AsCoreType(aSockName));
 }
 
+static uint8_t g_wbuf[64];
+
+otError __otUdpCoapSecure(
+	otInstance *aInstance,
+	otMessage **aMessage,
+	const otMessageInfo *aMessageInfo)
+{
+	otError err = OT_ERROR_NONE;
+	GroupOSCOREContext* gosc_ctx;
+	otMessage* outMessage;
+	otCoapOptionIterator coapOptionIt;
+	// Work buffer offset index.
+	uint16_t wi;
+	const uint8_t* myAddr;
+
+	// ===== Step 1: Form the OSCORE plaintext.
+	// The plaintext consists of:
+	// - E-class options
+	// - the code
+	// - the payload
+	// - AAD (OSCORE version no., AEAD algo. used, kid [sender ID], piv [partial IV])
+
+	// Collect all of the class E options.
+	VerifyOrExit((err = otCoapOptionIteratorInit(&coapOptionIt, *aMessage)) == OT_ERROR_NONE);
+	for (const otCoapOption* presentOption = otCoapOptionIteratorGetNextOption(&coapOptionIt);
+		 presentOption != NULL;
+		 presentOption = otCoapOptionIteratorGetNextOption(&coapOptionIt)) {
+
+	}
+
+	// Retrieve the Group OSCORE context based on the destination.
+	// TODO: correctly determine the Group OSCORE context.
+	gosc_ctx = AsCoreType(aInstance).GetGroupOSCOREContexts();
+
+	wi = 0;
+
+	// Add the AAD.
+	g_wbuf[wi++] = (4 << 5) | (4); // Array, 4 items.
+
+	// Item 1, OSCORE version.
+	g_wbuf[wi++] = (0b00000000) | 1; // OSCORE version => integer, 1.
+	// Item 2, Algorithms.
+	g_wbuf[wi++] = (0b01000000) | 4; // Algorithms => array, 4 items.
+	g_wbuf[wi++] = (0b00000000) | 10; // AEAD alg.: AES-CCM-16-64-128 => integer, 10.
+	g_wbuf[wi++] = (0b00000000) | 10; // Group enc. algo.: AES-CCM-16-64-128 => integer, 10.
+	g_wbuf[wi++] = (0b00111001); // Sig. Algo.: EdDSA => -8 -> 2-byte unsigned integer extension, 7
+	g_wbuf[wi++] = 0;
+	g_wbuf[wi++] = 7;
+	g_wbuf[wi++] = (0b00111001); // Pairwise key agreement: ECDH-SS + HKDF-256 => -27 -> 2-byte unsigned integer extension, 26
+	g_wbuf[wi++] = 0;
+	g_wbuf[wi++] = 26;
+	// Item 3, kid (key ID)/sender ID.
+    myAddr = otIp6GetUnicastAddresses(aInstance) // HACK; just use the first one.
+		->mAddress    // Get the address...
+		.mFields      // as divided fields...
+		.mComponents  // where the fields are the network prefix and the interface ID (IID)...
+		.mIid         // and we want the IID...
+		.mFields      // as divided fields...
+		.m8;          // where the fields are 8-bit values.
+	// The choice of the sender ID is user-defined.
+	// We use the lower 7 bits as the sender ID.
+	// The length is constrained to the nonce length - 6 (13 - 6 = 7).
+	for (uint8_t i = 1; i < 8; i++) { g_wbuf[wi++] = myAddr[i]; }
+	// Item 4, Partial IV (uses the sender sequence no.).
+	g_wbuf[wi++] = gosc_ctx->mSenderSequenceNumber++; // Increment the sequence no. for the next message.
+
+	// This is the transformed message the caller should work with.
+	*aMessage = NULL;
+
+exit:
+	return err;
+}
+
 otError otUdpSend(otInstance *aInstance, otUdpSocket *aSocket, otMessage *aMessage, const otMessageInfo *aMessageInfo)
 {
     otError error;
-	uint8_t first_byte_of_secret;
+	// uint8_t first_byte_of_secret;
 
     VerifyOrExit(!AsCoreType(aMessage).IsOriginThreadNetif(), error = kErrorInvalidArgs);
 
-	// Translate the CoAP message into an OSCORE message using the context available in otInstance.
-	first_byte_of_secret = AsCoreType(aInstance)
-		.GetGroupOSCOREContexts()[0].mMasterSecret[0];
+	// Translate the CoAP message into a Group OSCORE message.
+    error = __otUdpCoapSecure(
+		aInstance,
+		&aMessage,
+		aMessageInfo);
 
     error = AsCoreType(aInstance).Get<Ip6::Udp>().SendTo(AsCoreType(aSocket), AsCoreType(aMessage),
                                                          AsCoreType(aMessageInfo));
+
 exit:
     return error;
 }
